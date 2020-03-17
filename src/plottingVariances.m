3776768790166956d3fae70c9ca97b17b7a35327
Torstein Ørbeck Eliassen <torsteoe@stud.ntnu.no>
2020-03-17 17:17:24 +0100

    plot fixes

--- src/plottingVariances.m
+++ src/plottingVariances.m
@@ -14,19 +14,18 @@ for i = 1:num_snr_values
     [omegas_crlb(i), phis_crlb(i)] = get_CRLB(SNR_mag(i));
 end
 
-k_values = [10, 12, 14, 16, 18];
+k_values = [10, 12];
 variance_arrays_omega = [];
 variance_arrays_phi = [];
 for k = 1:size(k_values, 2)
-    variance_for_different_snr_omega = zeros(size(SNR, 2));
-    variance_for_different_snr_phi = zeros(size(SNR, 2));
+    variance_for_different_snr_omega = zeros(1, size(SNR, 2));
+    variance_for_different_snr_phi = zeros(1, size(SNR, 2));
     for j = 1:size(SNR, 2)
-        %variance_for_different_snr_omega(j) = get_var_omega(k, SNR(j))
-        %variance_for_different_snr_phi(j) = get_var_phi(k, SNR(j))
+        [variance_for_different_snr_omega(j),  variance_for_different_snr_phi(j)] = gen_var(SNR(j), k_values(k));
     end
     
-    variance_arrays_omega = [variance_arrays_omega; variance_for_different_snr_omega]
-    variance_arrays_phi = [variance_arrays_phi; variance_for_different_snr_phi]
+    variance_arrays_omega = [variance_arrays_omega; variance_for_different_snr_omega];
+    variance_arrays_phi = [variance_arrays_phi; variance_for_different_snr_phi];
 end
 
 
@@ -35,22 +34,22 @@ end
 
 figure()
 subplot(2, 1, 1)
-loglog(SNR_mag, omegas_crlb, 'black');
-for k = 1:size(variance_arrays_omega, 2)
+semilogy(SNR, omegas_crlb, 'black');
+for k = 1:size(k_values, 2)
     hold on;
-    loglog(SNR_mag, variance_arrays_omega(k,:));
+    semilogy(SNR, variance_arrays_omega(k,:));
 end
-legend('CRLB', 'k=10')
+legend('CRLB', 'k=10', 'k=12', 'k=14', 'k=16', 'k=18')
 
 
 subplot(2, 1, 2);
-loglog(SNR_mag, phis_crlb, 'black');
+semilogy(SNR, phis_crlb, 'black');
 hold on;
-for k = 1:size(variance_arrays_omega, 2)
+for k = 1:size(k_values, 2)
     hold on;
-    loglog(SNR_mag, variance_arrays_phi(k,:));
+    semilogy(SNR, variance_arrays_phi(k,:));
 end
-legend('CRLB')
+legend('CRLB', 'k=10', 'k=12', 'k=14', 'k=16', 'k=18')
 
 end
 
