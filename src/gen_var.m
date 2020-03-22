e6b1bcef067f3116fa6a763fb0bebe10dfd7ef11
torsteoe <47139578+torsteoe@users.noreply.github.com>
2020-03-22 18:56:07 +0100

    plot two graphs , matlab and our fft

--- src/gen_var.m
+++ src/gen_var.m
@@ -1,12 +1,12 @@
-function [var_omega_error, var_phi_error] = gen_var(SNR,k)
+function [var_omega_error, var_phi_error] = gen_var(SNR,k, directory)
 %GEN_VAR Summary of this function goes here
 %   Detailed explanation goes here
 mapstr = "SNR("+int2str(SNR)+")";
 mapstr = mapstr+"_k("+int2str(k)+")";
 
-omega_error_arr = load('../EstimationsFFTMatlab/'+mapstr+'/omega_error_arr.mat');
+omega_error_arr = load('../'+directory+'/'+mapstr+'/omega_error_arr.mat');
 omega_error_arr = omega_error_arr.omega_error_arr;
-phi_error_arr = load('../EstimationsFFTMatlab/'+mapstr+'/phi_error_arr.mat');
+phi_error_arr = load('../'+directory+'/'+mapstr+'/phi_error_arr.mat');
 phi_error_arr = phi_error_arr.phi_error_arr;
 
 var_omega_error = (omega_error_arr*omega_error_arr')/(size(omega_error_arr, 2)-1);
